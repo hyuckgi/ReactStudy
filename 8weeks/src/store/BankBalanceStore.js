4fd4382c6f944c237daad70841bc2ca8e1a5c55e
gadokkwon@gmail.com <gadokkwon@gmail.com>
2016-12-06 18:14:03 +0900

    Revert ""flux CHAPTER 2""
    This reverts commit 1cf11c499dfa1c31bfba882bab7af1dd3f3c8697.

--- 8weeks/src/store/BankBalanceStore.js
+++ 8weeks/src/store/BankBalanceStore.js
@@ -1,34 +1,37 @@
-
+import { EventEmitter } from "fbemitter";
 import AppDispatcher from '../AppDispatcher';
 import bankConstants from '../constants';
 
-//  Use Store
-import { Store } from 'flux/utils';
+const CHANGE_EVENT = 'change';
 
+let _emitter = new EventEmitter();
 let balance = 0;
 
-class BankBalanceStore extends Store{
+let BankBalnceStore = {
     getState(){
         return balance;
+    },
+    
+    addListener(callback){
+        return _emitter.addListener(CHANGE_EVENT, callback);
     }
+};
 
-    __onDispatch(action){
-        switch (action.type) {
-            case bankConstants.CREATED_ACCOUNT :
-                balance = 0;
-                this.__emitChange();
-                break;
-            case bankConstants.DEPOSITED_INTO_ACCOUNT :
-                balance = balance + action.amount;
-                this.__emitChange();
-                break;
-            case bankConstants.WITHDREW_FROM_ACCOUNT :
-                balance = balance - action.amount;
-                this.__emitChange();
-                break;
-        }
+BankBalnceStore.dispatchToken = AppDispatcher.register((action) => {
+    switch(action.type){
+        case bankConstants.CREATED_ACCOUNT :
+            balance = 0;
+            _emitter.emit(CHANGE_EVENT);
+            break;
+        case bankConstants.DEPOSITED_INTO_ACCOUNT :
+            balance = balance + action.amount;
+            _emitter.emit(CHANGE_EVENT);
+            break;
+        case bankConstants.WITHDREW_FROM_ACCOUNT :
+            balance = balance - action.amount;
+            _emitter.emit(CHANGE_EVENT);
+            break;
     }
-}
+});
 
-
-export default new BankBalanceStore(AppDispatcher);
+export default BankBalnceStore;
