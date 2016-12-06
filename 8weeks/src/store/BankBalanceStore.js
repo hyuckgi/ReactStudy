1cf11c499dfa1c31bfba882bab7af1dd3f3c8697
gadokkwon@gmail.com <gadokkwon@gmail.com>
2016-12-06 18:13:31 +0900

    "flux CHAPTER 2"

--- 8weeks/src/store/BankBalanceStore.js
+++ 8weeks/src/store/BankBalanceStore.js
@@ -1,37 +1,34 @@
-import { EventEmitter } from "fbemitter";
+
 import AppDispatcher from '../AppDispatcher';
 import bankConstants from '../constants';
 
-const CHANGE_EVENT = 'change';
+//  Use Store
+import { Store } from 'flux/utils';
 
-let _emitter = new EventEmitter();
 let balance = 0;
 
-let BankBalnceStore = {
+class BankBalanceStore extends Store{
     getState(){
         return balance;
-    },
-    
-    addListener(callback){
-        return _emitter.addListener(CHANGE_EVENT, callback);
     }
-};
 
-BankBalnceStore.dispatchToken = AppDispatcher.register((action) => {
-    switch(action.type){
-        case bankConstants.CREATED_ACCOUNT :
-            balance = 0;
-            _emitter.emit(CHANGE_EVENT);
-            break;
-        case bankConstants.DEPOSITED_INTO_ACCOUNT :
-            balance = balance + action.amount;
-            _emitter.emit(CHANGE_EVENT);
-            break;
-        case bankConstants.WITHDREW_FROM_ACCOUNT :
-            balance = balance - action.amount;
-            _emitter.emit(CHANGE_EVENT);
-            break;
+    __onDispatch(action){
+        switch (action.type) {
+            case bankConstants.CREATED_ACCOUNT :
+                balance = 0;
+                this.__emitChange();
+                break;
+            case bankConstants.DEPOSITED_INTO_ACCOUNT :
+                balance = balance + action.amount;
+                this.__emitChange();
+                break;
+            case bankConstants.WITHDREW_FROM_ACCOUNT :
+                balance = balance - action.amount;
+                this.__emitChange();
+                break;
+        }
     }
-});
+}
 
-export default BankBalnceStore;
+
+export default new BankBalanceStore(AppDispatcher);
