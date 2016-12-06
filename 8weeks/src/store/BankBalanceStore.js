fc3c6a1cb361726626b6708e4d83459918c1cb63
gadokkwon@gmail.com <gadokkwon@gmail.com>
2016-12-06 18:18:28 +0900

    "flux CHAPTER 3"

--- 8weeks/src/store/BankBalanceStore.js
+++ 8weeks/src/store/BankBalanceStore.js
@@ -1,37 +1,57 @@
-import { EventEmitter } from "fbemitter";
+
 import AppDispatcher from '../AppDispatcher';
 import bankConstants from '../constants';
 
-const CHANGE_EVENT = 'change';
+//  Use Store
+// import { Store } from 'flux/utils';
+//
+// let balance = 0;
+//
+// class BankBalanceStore extends Store{
+//     getState(){
+//         return balance;
+//     }
+//
+//     __onDispatch(action){
+//         switch (action.type) {
+//             case bankConstants.CREATED_ACCOUNT :
+//                 balance = 0;
+//                 this.__emitChange();
+//                 break;
+//             case bankConstants.DEPOSITED_INTO_ACCOUNT :
+//                 balance = balance + action.amount;
+//                 this.__emitChange();
+//                 break;
+//             case bankConstants.WITHDREW_FROM_ACCOUNT :
+//                 balance = balance - action.amount;
+//                 this.__emitChange();
+//                 break;
+//         }
+//     }
+// }
 
-let _emitter = new EventEmitter();
-let balance = 0;
+//  Use ReduceStore
+import { ReduceStore } from 'flux/utils';
 
-let BankBalnceStore = {
-    getState(){
-        return balance;
-    },
-    
-    addListener(callback){
-        return _emitter.addListener(CHANGE_EVENT, callback);
+
+class BankBalanceStore extends ReduceStore{
+    getInitialState(){
+        return 0;
     }
-};
 
-BankBalnceStore.dispatchToken = AppDispatcher.register((action) => {
-    switch(action.type){
-        case bankConstants.CREATED_ACCOUNT :
-            balance = 0;
-            _emitter.emit(CHANGE_EVENT);
-            break;
-        case bankConstants.DEPOSITED_INTO_ACCOUNT :
-            balance = balance + action.amount;
-            _emitter.emit(CHANGE_EVENT);
-            break;
-        case bankConstants.WITHDREW_FROM_ACCOUNT :
-            balance = balance - action.amount;
-            _emitter.emit(CHANGE_EVENT);
-            break;
+    reduce(state, action){
+        switch (action.type) {
+            case bankConstants.CREATED_ACCOUNT :
+                return 0;
+
+            case bankConstants.DEPOSITED_INTO_ACCOUNT :
+                return state + action.amount;
+
+            case bankConstants.WITHDREW_FROM_ACCOUNT :
+                return state - action.amount;
+        }
     }
-});
+}
 
-export default BankBalnceStore;
+
+export default new BankBalanceStore(AppDispatcher);
